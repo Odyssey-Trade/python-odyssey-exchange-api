3cba0699f325ed269900827e72718174b1cc3229
goduni <xxlordbunk150@gmail.com>
2024-10-25 15:58:16 +0300

    updated setup.py and requirements.txt

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@ import setuptools
 
 setuptools.setup(
     name="odyssey_exchange_api",
-    version="0.0.1",
-    author="author",
-    description="",
-    packages=[
-        "odyssey_exchange_api",
-        "odyssey_exchange_api.clients",
-        "odyssey_exchange_api.enums",
-        "odyssey_exchange_api.exceptions",
-        "odyssey_exchange_api.objects",
-        "odyssey_exchange_api.requests",
-        "odyssey_exchange_api.responses",
+    version="0.0.5",
+    author="goduni",
+    url="https://github.com/Odyssey-Trade/python-odyssey-exchange-api",
+    description="Library for working with odyssey.trade",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages("."),
+    python_requires=">=3.10",
+    install_requires=[
+        "pydantic==2.9.2",
+        "httpx==0.27.2",
+        "websockets==13.1"
     ],
-    python_requires=">=3.10"
+    license="MIT"
 )
